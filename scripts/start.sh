c82cb85339d919f4bcb81f2d0eb3b72d60055894
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-03 09:47:03 +0900

    feat:start script 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "[App Start]================================"
+cd /home/ec2-user/lgcns_data_manager
+
 sudo docker-compose down --volumes --rmi all
 
 sudo docker-compose up airflow-init
