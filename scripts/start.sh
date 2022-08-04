9f6839d3932c841eea211343aaf0ab873bd9b1d3
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-04 14:20:20 +0900

    feat:webserver port 변경 and start script 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "[App Start]================================"
-cd /home/ec2-user/lgcns_data_manager
 
-sudo docker-compose up airflow-init
-sudo docker-compose up -d
+ENV=$1
+
+if [ $ENV = "local" ]
+then
+    echo "Local Start !!"
+    docker-compose up airflow-init
+    docker-compose up -d
+else
+    echo "Prod Start !!"
+    cd /home/ec2-user/lgcns_data_manager
+    sudo docker-compose up airflow-init
+    sudo docker-compose up -d
+fi
